bc2e038c4bf83fccb09b442e90196921daaad12c
Tarun <tannu.2354@gmail.com>
2023-03-16 00:27:35 -0700

    Added Single GPU

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
 
 def save_config_file(model_checkpoints_folder, args):
     if not os.path.exists(model_checkpoints_folder):
-        os.makedirs(model_checkpoints_folder)
+        os.makedirs(model_checkpoints_folder, exist_ok=True)
         with open(os.path.join(model_checkpoints_folder, 'config.yml'), 'w') as outfile:
             yaml.dump(args, outfile, default_flow_style=False)
 
